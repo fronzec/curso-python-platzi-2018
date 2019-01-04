5b10acaaf1ca0ab6c221f1a1d8a51d7ae9b7ae5d
Eduardo C. Flores Ambrosio <eflores@rappi.com>
2019-01-04 01:51:35 -0600

    add: Estructuras condicionales en el proyecto

--- proyecto/main.py
+++ proyecto/main.py
@@ -4,8 +4,11 @@ clientes = 'pablo,ricardo,'
 def create_client(client_name):
     # Permite usar una variable global dentro de la funcion
     global clientes
-    clientes += client_name
-    _add_comma()
+    if client_name not in clientes:
+        clientes += client_name
+        _add_comma()
+    else:
+        print("El cliente ya esta en la lista de clientes")
 
 
 def list_clients():
@@ -18,7 +21,23 @@ def _add_comma():
     clientes += ','
 
 
+def _print_welcome():
+    print('Bienvenidos a la app de ventas')
+    print('*' * 50)
+    print('¿Que quisieras hacer hoy?')
+    print('[C]reate client')
+    print('[D]elete client')
+    print('')
+
+
 if __name__ == '__main__':
-    list_clients()
-    create_client('david')
-    print(clientes)
+    _print_welcome()
+    command = input()
+    if command == 'C':
+        client_name = input("¿Cual es el nombre del cliente? ")
+        create_client(client_name)
+        list_clients()
+    elif command == 'D':
+        pass
+    else:
+        print("Invalid command")
