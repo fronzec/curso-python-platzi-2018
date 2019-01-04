b6680d7b4e30da3dea4cd960e0e40575b4736ecc
Eduardo C. Flores Ambrosio <eflores@rappi.com>
2019-01-04 01:35:39 -0600

    add: Funciones en el proyecto

--- proyecto/main.py
+++ proyecto/main.py
@@ -1,5 +1,24 @@
 clientes = 'pablo,ricardo,'
 
+
+def create_client(client_name):
+    # Permite usar una variable global dentro de la funcion
+    global clientes
+    clientes += client_name
+    _add_comma()
+
+
+def list_clients():
+    global clientes
+    print(clientes)
+
+
+def _add_comma():
+    global clientes
+    clientes += ','
+
+
 if __name__ == '__main__':
-    clientes += 'david'
+    list_clients()
+    create_client('david')
     print(clientes)
